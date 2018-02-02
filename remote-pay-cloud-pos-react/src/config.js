6f987a1c0f65b8cfd144b5c0a9cee1a29d3abfef
borissedov <borissedov@gmail.com>
2018-02-02 17:41:30 +0300

    minor fix

--- remote-pay-cloud-pos-react/src/config.js
+++ remote-pay-cloud-pos-react/src/config.js
@@ -1,10 +1,10 @@
 export const myConfig = {
     appId: 'com.clover.cloud-pos-example-react',
     clientId: 'HBK8YZG9EQNJG',
-    cloverServer: 'https://dev1.dev.clover.com/'
+    cloverServer: 'https://dev1.dev.clover.com/',
     devicesDomain: 'https://apidev1.dev.clover.com/v3/merchants/',
     oAuthDomain: 'https://dev1.dev.clover.com',
-    posName: 'pos.name'        
+    posName: 'pos.name',
     serialNumber: 'register_1',
     uriText: 'wss://10.249.254.214:12345/remote_pay'
 };
